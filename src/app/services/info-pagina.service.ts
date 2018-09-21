76e3fb8676fe77a9ec1ef133d7a0838facc11a7d
Omar Gutierrez <gutzoma@gmail.com>
2018-09-21 12:11:38 -0500

    tama version 3

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,18 +9,33 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
-
+  equipo: any[] = [];
   constructor( private http: HttpClient ) {
     // console.log('Servicio de infoPagina listo');
 
+   this.cargarInfo();
+   this.cargarEquipo();
+   }
+
+   private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina) => {
 
       this.cargada = true;
       this.info = resp;
 
-      console.log(resp);
+    });
+   }
+
+   private cargarEquipo(){
+    this.http.get('https://angula-html.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+
+      this.equipo = resp;
+
+      // console.log(resp);
     });
 
    }
+
 }
